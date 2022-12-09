a096d9f71cfc0dbe5b47cc5193eae408a60f0bbc
Lizzy <sizzyhart@gmail.com>
2022-12-09 12:14:54 -0700

    Alien Ships

---
+++ UFO Defense Force Game/Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    private float spawnRangeX = 14f, spawnPosZ = 40f, startDelay = 2f, spawnInterval = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval);
+    }
+
+    void SpawnRandomEnemy()
+    {
+        // Generate random position to spawn at
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        // pick random enemy ship
+        int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+        // spawn enemy
+        Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+    }
+}
